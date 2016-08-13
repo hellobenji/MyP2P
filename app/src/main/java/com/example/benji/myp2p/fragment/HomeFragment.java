f2caee8a888fe8520293de27a0a0b195fd1ac4ec
hellobenji <863360109@qq.com>
2016-08-13 20:11:42 +0800

    头布局初始化完成

--- app/src/main/java/com/example/benji/myp2p/fragment/HomeFragment.java
+++ app/src/main/java/com/example/benji/myp2p/fragment/HomeFragment.java
@@ -7,19 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.benji.myp2p.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benji on 2016/8/12.
  */
 public class HomeFragment extends Fragment {
+    @BindView(R.id.iv_title_back)
+    ImageView ivTitleBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.iv_title_setting)
+    ImageView ivTitleSetting;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = View.inflate(container.getContext(), R.layout.mainfragmetn, null);
+        ButterKnife.bind(this, view);
+        //初始化头布局
+        initTittle();
         return view;
 
         //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initTittle() {
+        tvTitle.setText("首页");
+        ivTitleBack.setVisibility(View.GONE);
+        ivTitleSetting.setVisibility(View.GONE);
     }
 }
--- app/src/main/java/com/example/benji/myp2p/fragment/MyvalueFragment.java
+++ app/src/main/java/com/example/benji/myp2p/fragment/MyvalueFragment.java
@@ -7,19 +7,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.benji.myp2p.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benji on 2016/8/12.
  */
 public class MyvalueFragment extends Fragment {
+    @BindView(R.id.iv_title_back)
+    ImageView ivTitleBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.iv_title_setting)
+    ImageView ivTitleSetting;
+    @BindView(R.id.textView)
+    TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = View.inflate(container.getContext(), R.layout.myvaluefragmetn, null);
+        ButterKnife.bind(this, view);
+        //初始化头布局
+        initTittle();
         return view;
+    }
 
-        //return super.onCreateView(inflater, container, savedInstanceState);
+    private void initTittle() {
+        tvTitle.setText("我的资产");
+        ivTitleBack.setVisibility(View.GONE);
+        ivTitleSetting.setVisibility(View.GONE);
+
     }
 }
